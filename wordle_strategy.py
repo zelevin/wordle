67862ec8c33eec40d49bd25917de2534ccbc8bfd
Vladimir Zelevinsky <vladimir.git@mailnull.com>
2022-01-17 20:10:20 -0500

    Add files via upload

--- wordle_strategy.py
+++ wordle_strategy.py
@@ -5,24 +5,41 @@ from collections import defaultdict
 all_words = [word.strip() for word in open('wordle_all.txt')]
 all_scores = np.load('wordle.npz')['scores']
 
+def score_to_str(s):
+    return ''.join('_YG'[s // 3 ** i % 3] for i in range(5))
+
 def analyze(first):
     groups = defaultdict(list)
     for secret in range(len(all_words)):
         groups[all_scores[secret, first]] += [secret]        
-    strategy = {first_score: (1.0, secrets[0]) if len(secrets) == 1 else
-        max((len(set(all_scores[secret, j] for secret in secrets)) / len(secrets), j) for j in range(len(all_words)))
-        for first_score, secrets in groups.items()}
-    return strategy, sum(strategy[first_score][0] * len(secrets) for first_score, secrets in groups.items()) / len(all_words)
-
-def score_to_str(s):
-    return ''.join('_YG'[s // 3 ** i % 3] for i in range(5))
+    strategy, total_count = {}, 0
+    for first_score, secrets in groups.items(): 
+        third = ''
+        if len(secrets) == 1:
+            count, second = 1.0, secrets[0]
+        else:
+            best_groups = {}
+            for j in range(len(all_words)):
+                candidates = defaultdict(list)
+                for secret in secrets:
+                    candidates[all_scores[secret, j]] += [secret]
+                if len(candidates) > len(best_groups):
+                    best_groups, best_second = candidates, j
+                if len(candidates) == len(secrets):
+                    break
+            count, second = len(best_groups), best_second
+            if count == len(secrets):
+                third = ', '.join(score_to_str(k) + ': ' + all_words[v[0]] for k, v in best_groups.items())
+        strategy[first_score] = count / len(secrets), second, third
+        total_count += count
+    return strategy, total_count / len(all_words)
 
 for first in argv[1:]:
     if first in all_words:
         strategy, prob = analyze(all_words.index(first))
         print('{} = {:.4f}'.format(first, prob))
         if len(argv) == 2:
-            for k, (prob, second) in sorted(strategy.items()):
-                print('{} {} {:.4f}'.format(score_to_str(k), all_words[second], prob))
+            for k, (prob, second, third) in sorted(strategy.items()):
+                print('{} {} {:.4f} {}'.format(score_to_str(k), all_words[second], prob, third))
     else:
         print('\'{}\' is not a permitted word'.format(first))
